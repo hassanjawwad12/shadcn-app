2f915439ebd13ba4c24307800c83b258df8e78b6
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2025-01-07 12:10:48 +0500

    ⚡Initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col w-full  items-center justify-center sm:items-start min-h-screen">
+     
+      <h1>Hello </h1>
+    </main>
+  );
+}
