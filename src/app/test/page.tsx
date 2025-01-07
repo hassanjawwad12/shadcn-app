c6b23ffe3716052456c9196cb1afcf9d47f51fe6
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2025-01-07 12:31:51 +0500

    ⚡Added Custom Chai variant in Button

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
-    <div className='bg-gray-800 text-white text-4xl min-h-screen w-full  flex flex-col items-center justify-center'>page</div>
+    <div className='bg-gray-800 text-white text-4xl min-h-screen w-full  flex flex-col items-center justify-center'>
+        <Button variant='chai' size='lg'>Shadcn button</Button>
+    </div>
   )
 }
 
