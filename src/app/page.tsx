24582ea93fda4e2e1d9a37049df4552783b5bbe8
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2025-01-07 12:22:40 +0500

    ⚡Initialized shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full  items-center justify-center sm:items-start min-h-screen">
+    <main className="flex flex-col w-full items-center justify-center min-h-screen bg-black">
      
       <h1>Hello </h1>
     </main>
---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-gray-800 text-white text-4xl min-h-screen w-full  flex flex-col items-center justify-center'>page</div>
+  )
+}
+
+export default page
