4235387f11033a4d36682b97ad428d04b87b3c2e
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2025-01-07 12:42:13 +0500

    ⚡Added Alert

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,6 +6,17 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 
 const page = () => {
@@ -45,6 +56,25 @@ const page = () => {
                     ))}
                 </Accordion>
             </main>
+            <AlertDialog>
+                <AlertDialogTrigger asChild>
+                <Button variant='chai' size='lg'>Open</Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+
         </div>
     )
 }
